1582e9d6774422fb803feddc1455e5e4966e92b6
mozbeel <matthiasleeb4@gmail.com>
2025-06-21 20:01:02 +0200

    Enhanced android building scripts

--- scripts/build-android.bat
+++ scripts/build-android.bat
@@ -2,6 +2,7 @@
 title iLuma Build Script for Android
 
 set debug_or_release=%~1
+set /p bundle_or_apk=Enter to build APK or App Bundle (apk/bundle; defaults to apk):
 
 if not exist gradlew.bat (
     echo gradlew.bat not found in %cd%
@@ -9,11 +10,21 @@ if not exist gradlew.bat (
 )
 
 if "%debug_or_release%"=="release" (
-    call gradlew.bat assembleRelease
-    set exitCode=%ERRORLEVEL%
+    if "%bundle_or_apk%"=="bundle" (
+        echo Building App Bundle in Release mode...
+        call gradlew.bat bundleRelease
+    ) else (
+        echo Building APK in Release mode...
+        call gradlew.bat assembleRelease
+    )
 ) else if "%debug_or_release%"=="debug" (
-    call gradlew.bat assembleDebug
-    set exitCode=%ERRORLEVEL%
+    if "%bundle_or_apk%"=="bundle" (
+        echo Building App Bundle in Debug mode...
+        call gradlew.bat bundleDebug
+    ) else (
+        echo Building APK in Debug mode...
+        call gradlew.bat assembleDebug
+    )
 ) else (
     echo Invalid argument. Use 'release' or 'debug'.
     exit /b 1
